2cc013333b6c8064392752414b599b317779ab52
theRMH <142658796+theRMH@users.noreply.github.com>
2025-10-24 16:33:25 +0530

    Update outputFileTracingRoot to use __dirname

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  outputFileTracingRoot: path.resolve(__dirname, '../../'),
+  outputFileTracingRoot: __dirname,
   typescript: {
     ignoreBuildErrors: true,
   },
